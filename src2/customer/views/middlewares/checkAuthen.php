c00cd14185cdb6ca07644d129cc55b75e370f106
Hồ Nguyễn Minh Khoa <mkhoa639@gmail.com>
2025-05-16 01:14:30 +0700

    ...

--- src2/customer/views/middlewares/checkAuthen.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start(); // Chỉ khởi tạo session nếu chưa được khởi tạo
-}
-
-if (!isset($_SESSION['user_id'])) {
-    header("Location: ?page=login");
-    exit();
-}
--- src2/customer/views/pages/home.php
+++
@@ -1,6 +0,0 @@
-<link rel="stylesheet" href="customer/views/assets/css/home.css">
-
-<?php
-    include("customer/views/layout/banner.php");
-    include("customer/views/layout/products_section.php");
-?>
